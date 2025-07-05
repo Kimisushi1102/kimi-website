008215ce8de91f700f8692a36c57d37c766f9ee1
Kimisushi1102 <kimisushi.restaurant@gmail.com>
2025-07-05 13:01:32 +0200

    Add files via upload

---
+++ app/(home)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Willkommen bei Kimi Sushi</h1>
+      <p>Sorgfalt in jedem Schnitt – Genuss in jedem Biss</p>
+    </main>
+  );
+}
