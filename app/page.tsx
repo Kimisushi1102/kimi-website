f56a6d5b620f2a17cc36a73a637f2f9cf063f025
Kimisushi1102 <kimisushi.restaurant@gmail.com>
2025-07-05 12:58:24 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/page.tsx
-export default function HomePage() {
-  return (
-    <main>
-      <h1>Willkommen bei Kimi Sushi</h1>
-      <p>Sorgfalt in jedem Schnitt – Genuss in jedem Biss</p>
-    </main>
-  );
-}
